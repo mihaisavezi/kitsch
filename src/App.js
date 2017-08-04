68564929cabc84dff6632c8e41fbc72b52717895
MIHAI POPESCU <mihai.ppscu@gmail.com>
2017-08-04 08:04:40 +0300

    add more descriptive text for product. Adjust copy.

--- src/App.js
+++ src/App.js
@@ -1,76 +1,123 @@
-import React, { Component } from 'react';
-import 'tachyons';
-import './App.css';
-import product from './product_03.jpg';
-import Header from './components/Header'
-import Form from './components/Form'
-import { ModalContainer, ModalDialog } from 'react-modal-dialog';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "tachyons";
+import "./App.css";
+import product from "./product_03.jpg";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import { ModalContainer, ModalDialog } from "react-modal-dialog";
 
 let ga = window.ga;
 
 class Hero extends Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			isShowingModal: false,
-		}
-	}
+    this.state = {
+      isShowingModal: false
+    };
+  }
 
-	handleClick = () => {
-		ga('send', 'event', 'buy button', 'click', 'esenta de hamsii');		
-		this.setState({ isShowingModal: true })
-	}
-	handleClose = () => this.setState({ isShowingModal: false })
-
-	
-	
-
-	render() {
-
-		const modalStyle = {
-			padding: '40px 40px',
-			minWidth: 400,
-			borderRadius: '0'
-		}
-
-
-		return (<div className="w-100 flex flex-column items-center justify-center">
-			<img className="mb3" src={process.env.PUBLIC_URL + '/product-logo.svg'} alt="" />
-			<h3 className="f3 fw6 ma0 mb4 kitsch-brown avenir">Avem, avem hamsi aveeem!</h3>
-			<p className="f5 ma0 mb5 kitsch-brown lh-copy tc avenir measure">
-				Stiu ca iti amintesti cu drag de mirosul puternic de hamsii si usturoi in diminetiile calduroase cand mergeai la plaja. Acum nu mai trebuie sa te duci pe faleza pentru o hamsie, vine hamsia la tine.
-    </p>
-			<img className="vh-75 mh-3" src={product} alt="" />
-			<a onClick={this.handleClick} className="f5 avenir link dim ph4 pv3 mb1 mt4 dib white bg-darken-kitsch-brown">Comandă Acum</a>
-			{/* <p className="f6 mb4 kitsch-brown">20 RON / Sticlă</p> */}
-			{
-				this.state.isShowingModal &&
-				<ModalContainer onClose={this.handleClose}>
-					<ModalDialog style={modalStyle} onClose={this.handleClose}>
-						<Form/>
-					</ModalDialog>
-				</ModalContainer>
-			}
-		</div>
-		)
-	}
-}
-
-class App extends Component {
-	constructor(props) {
-		super(props);
-
-	}
+  handleClick = () => {
+    ga("send", "event", "buy button", "click", "esenta de hamsii");
+    this.setState({ isShowingModal: true });
+  };
+  handleClose = () => this.setState({ isShowingModal: false });
 
   render() {
+    const modalStyle = {
+      padding: "40px 40px",
+      minWidth: 400,
+      borderRadius: "0"
+    };
+
     return (
-      <div className="App mw9 ph6 mt2 mb4 center">
-        <Header/> 
-        <Hero/>
+      <div className="w-100 flex flex-column items-center justify-center">
+        {/* <h3 className="f-subheadline lh-title measure-narrow w-50 tc center kitsch-brown ma0 mb4">
+          Esențe neobișnuite.
+        </h3> */}
+        <img
+          className="mb3"
+          src={process.env.PUBLIC_URL + "/product-logo.svg"}
+          alt=""
+        />
+        <h3 className="f3 fw6 ma0 kitsch-brown avenir">
+          Eau de Costinești - Elimină mirosurile plăcute.
+        </h3>
+        <p className="f6 tc ttu tracked ma0 mt2 mb4 kitsch-brown o-60 avenir measure-wide">
+          odorizant de cameră
+        </p>
+        <p className="f5 ma0 mb5 kitsch-brown lh-copy tc avenir measure">
+          Se spune că omul sfințește locul. Însă noi credem că mirosul îl
+          sfințește. Și avem și dovada vie. Esența de hamsie, extrasă cu grija
+          din sfintele grătare din Costinesti e tot ce ai nevoie ca sa aduci
+          amintirile unei veri toride, la tine în casă. Pentru un efect de
+          durată asigură-te că dai în toată casa, atât în bucătarie dar mai ales
+          în dormitor.
+        </p>
+        <img className="vh-75 mh-3" src={product} alt="" />
+        <p className="f6 mb4 kitsch-brown o-6">20 RON / Sticlă</p>
+        <a
+          onClick={this.handleClick}
+          className="f5 avenir link dim ph4 pv3 mb2 mt4 dib white bg-darken-kitsch-brown"
+        >
+          Comandă Acum
+        </a>
+        <p className="f6 tc mb4 kitsch-brown o-60 avenir measure-wide">
+          Rezerva de microspray contine un parfum concentrat care se raspândește
+          sub forma unor particule fine în atmosferă.
+        </p>
+        {this.state.isShowingModal &&
+          <ModalContainer onClose={this.handleClose}>
+            <ModalDialog style={modalStyle} onClose={this.handleClose}>
+              <Form />
+            </ModalDialog>
+          </ModalContainer>}
       </div>
     );
   }
 }
 
-export default App;
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App min-vh-100 flex flex-column mw9 ph6 center">
+        <Header />
+        <Hero />
+      </div>
+    );
+  }
+}
+
+const WorkInProgress = () =>
+  <div className="App min-vh-100 flex flex-column mw9 ph6 center">
+    <Header />
+    <div className="w-100 flex flex-column flex-auto items-center justify-center">
+      <h1 className="f-headline ttu avenir fw3 tc lh-solid ma0 black-10 pa2 mb7">
+        Mai e de lucru.
+      </h1>
+    </div>
+  </div>;
+
+class Root extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={App} />
+          <Route component={WorkInProgress} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Root;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,37 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from "react";
 
+const Form = () =>
+  <div>
+    <form
+      action="https://kitsch-scents.us16.list-manage.com/subscribe/post"
+      method="POST"
+    >
+      <input type="hidden" name="u" value="065cd8f9c86b909964d163de1" />
+      <input type="hidden" name="id" value="4567245c2d" />
 
-const Form = () => (
-    <div>
-        <form action="https://kitsch-scents.us16.list-manage.com/subscribe/post" method="POST">
-            <input type="hidden" name="u" value="065cd8f9c86b909964d163de1"/>
-            <input type="hidden" name="id" value="4567245c2d"/>
+      <div className="avenir measure mb4">
+        <p className="f5 db mb4">
+          Fii printre primii care isi vor primi sticluta atunci cand vom lansa.
+        </p>
+        <label htmlFor="name" className="f5 db b mb2">
+          Email<span className="normal kitsch-brown" />
+        </label>
+        <input
+          id="name"
+          name="MERGE0"
+          className="input-reset ba b--black-20 pa2 mb2 db w-100"
+          type="text"
+          aria-describedby="name-desc"
+        />
+      </div>
 
-            <div className="avenir measure mb4">
-                <p className="f5 db mb4">Mai avem niste detalii de pus la punct si te vom anunța când suntem gata.</p>
-                <label htmlFor="name" className="f5 db b mb2">Email<span className="normal kitsch-brown"></span></label>
-                <input id="name" name="MERGE0" className="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc"/>
-            </div>
+      <input
+        type="submit"
+        className="ph4 pv3 input-reset ba b--white bg-darken-kitsch-brown white pointer f5 avenir"
+        name="submit"
+        value="Anunță-mă"
+      />
+    </form>
+  </div>;
 
-            <input type="submit" className="ph4 pv3 input-reset ba b--white bg-darken-kitsch-brown white pointer f5 avenir" name="submit" value="Anunță-mă" />
-            
-        </form>
-    </div>   
-)
-
-export default Form
-
+export default Form;
