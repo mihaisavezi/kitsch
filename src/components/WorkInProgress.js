6a52dc01324bd61d7546bd560942a84367c3ed75
MIHAI POPESCU <mihai.ppscu@gmail.com>
2017-08-06 19:57:29 +0300

    add small fixes

--- src/components/WorkInProgress.js
+++ src/components/WorkInProgress.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Header from './Header';
+import Header from "./Header";
 
 const WorkInProgress = () =>
-  <div className="App min-vh-100 flex flex-column mw9 ph6 center">
+  <div className="App min-vh-100 flex flex-column mw9 ph3 ph4-m ph6-l center">
     <Header />
     <div className="w-100 flex flex-column flex-auto items-center justify-center">
-      <h1 className="f-headline ttu avenir fw1 tc lh-solid ma0 kitsch-brown o-20 pa2 mb7">
+      <h1 className="f1 f-headline-l ttu avenir fw1 tc lh-solid ma0 kitsch-brown o-20 pa2 mb7">
         Mai e de lucru.
       </h1>
     </div>
   </div>;
 
-  export default WorkInProgress;
+export default WorkInProgress;
